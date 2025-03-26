039f0f2bbb1aa8473de9a81274d3511de9a8a394
tonyguo <tonyguo2010@gmail.com>
2025-03-25 21:30:04 -0400

    clean up

--- tester.go
+++ tester.go
@@ -1,19 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"libs"
-)
-
 func main() {
-	// s := "Welcome,to,GeeksforGeeks"
-	// fmt.Println("", s)
-
-	// result := strings.SplitAfter(s, ",")
-	// fmt.Println("Result:", result[1])
-	js := libs.LoadJsonFromFile("timing.json")
-	fmt.Println(js)
-
-	fmt.Println(js["main_loop"])
-	// fmt.Println(js["pre_input"])
 }
